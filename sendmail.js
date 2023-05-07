11b404b5a12a88bc04ff1ba30aef0ae525a62ab2
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-05-07 21:13:50 +0530

    email

--- sendmail.js
+++ sendmail.js
@@ -16,15 +16,17 @@ const sendEmail = async options => {
     const mailOptions = {
         // from: 'akshay.pranav.kalathil@gmail.com',
         from: 'chatbotusingchatgpt@gmail.com',
-        to: options.email,
-        // to: 'akshay.kalathil.pranav@gmail.com',
+        // to: options.email,
+        to: 'akshay.kalathil.pranav@gmail.com',
         // to: 'aqibshk28@gmail.com',
         // to: 'aqibshk28@gmail.com',
         subject: "Download Your resume",
         text: "Thank you for choosing us. Here is your resume",
         envelope: {
-            from: 'akshay.pranav.kalathil@gmail.com',
-            to: 'akshay.kalathil.pranav@gmail.com'
+            from: 'chatbotusingchatgpt@gmail.com',
+            to: options.email
+
+            // to: 'akshay.kalathil.pranav@gmail.com'
         },
         attachments: [
             {
